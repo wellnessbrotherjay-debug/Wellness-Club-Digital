f4a67b0e605a011aa89daac70cbbe65f25eec8d5
Samuel Jason <sammyjason39@gmail.com>
2026-03-11 12:35:06 +0800

    chore: remove redundant src/src directory and cleanup monorepo structure

--- apps/web/src/src/constants/config.ts
+++
@@ -1,2 +0,0 @@
-// Canonical Google Apps Script URL - matches .env.local APPS_SCRIPT_URL
-export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyLCafm3ltwnO1pNhEocaWYABSjV4Yxvn1yfXkOKohBv_JTxYu2buWRq51vjhPBX1JL/exec';
